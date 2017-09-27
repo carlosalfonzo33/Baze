790c3c1aa41e84898f84e01d4dc664358d2ebaf4
Sarah Lessner <sarahlessner@gmail.com>
2017-09-26 22:18:33 -0700

    Revert "configured props for stations.js - added full stations list"

--- client/src/components/Form/stationmenu.js
+++ client/src/components/Form/stationmenu.js
@@ -2,55 +2,19 @@ import React from "react";
 
 // var stations = ["12th St. Oakland City Center", "16th St Mission", "19th St. Oakland", "24th St. Mission", "Ashby (Berkeley)", "Balboa Park"];
 
-export const Stationmenu = props => (
-	<select {...props}>
-		<option>12th St. Oakland City Center</option>
-		<option>16th St. Mission (SF)</option>
-		<option>19th St. Oakland</option>
-		<option>24th St. Mission</option>
-		<option>Ashby (Berkeley)</option>
-		<option>Balboa Park (SF)</option>
-		<option>Bay Fair (San Leandro)</option>
-		<option>Castro Valley</option>
-		<option>Civic Center/UN Plaza</option>
-		<option>Coliseum</option>
-		<option>Colma</option>
-		<option>Concord</option>
-		<option>Daly City</option>
-		<option>Downtown Berkeley</option>
-		<option>Dublin/Pleasanton</option>
-		<option>El Cerrito Del Norte</option>
-		<option>El Cerrito Plaza</option>
-		<option>Embarcadero(SF)</option>
-		<option>Fremont</option>
-		<option>Fruitvale(Oakland)</option>
-		<option>Glen Park(SF)</option>
-		<option>Hayward</option>
-		<option>Lafayette</option>
-		<option>Lake Merritt(Oakland)</option>
-		<option>MacArthur(Oakland)</option>
-		<option>Millbrae</option>
-		<option>Montgomery St (SF)</option>
-		<option>North Berkeley</option>
-		<option>North Concord/Martinez</option>
-		<option>Oakland International Airport Station</option>
-		<option>Orinda</option>
-		<option>Pittsburg/Bay Point</option>
-		<option>Pleasant Hill / Contra Costa Centre</option>
-		<option>Powell St (SF)</option>
-		<option>Richmond</option>
-		<option>Rockridge(Oakland)</option>
-		<option>San Bruno</option>
-		<option>San Francisco International Airport</option>
-		<option>San Leandro</option>
-		<option>South Hayward</option>
-		<option>South San Francisco</option>
-		<option>Union City</option>
-		<option>Walnut Creek</option>
-		<option>Warm Springs/South Fremont</option>
-		<option>West Dublin/Pleasanton</option>
-		<option>West Oakland</option>
-	</select>
-);
+
+export const Stationmenu = props =>
+
+
+<select {...props}>
+    <option>12th St. Oakland City Center</option>
+    <option>16th St. Mission</option>
+    <option>19th St. Oakland</option>
+    <option>24th St. Mission</option>
+    <option>16th St. Mission</option>
+    <option>Ashby (Berkeley)</option>
+    <option>Balboa Park</option>
+
+</select>;
 
 export default Stationmenu;
