49b91d223311333f14d3b08447aae0f2a82b2388
Lopez <carlos.alfonzo@gmail.com>
2017-09-27 22:23:49 -0700

    added componont for createPost

--- client/src/components/Feednav/Navbar.js
+++ client/src/components/Feednav/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import CreatePost from '../CreatePost';
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props =>
@@ -11,26 +12,28 @@ const Navbar = props =>
           Bart Feed
         </Link>
       </div>
-      <ul className="nav navbar-nav">
-        <li
-          className={
-            window.location.pathname === "/feed" ? "active" : ""}>
-          <Link to="/feed">All</Link>
-        </li>
-        <li className={window.location.pathname === "/feed/delayfeed" ? "active" : ""}>
-          <Link to="/feed/delayfeed">Delay Posts</Link>
-        </li>
-        <li className={window.location.pathname === "/feed/station" ? "active" : ""}>
-          <Link to="/feed/station">Station Posts</Link>
-        </li>
-        <li className={window.location.pathname === "/feed/train" ? "active" : ""}>
-          <Link to="/feed/train">Train Posts</Link>
-        </li>
-        <li className={window.location.pathname === "/feed/myfeed" ? "active" : ""}>
-          <Link to="/feed/myfeed">My Posts</Link>
-        </li>
-
-      </ul>
+      <div className="collapse navbar-collapse" id="feedNav">
+        <ul className="nav navbar-nav">
+          <li
+            className={
+              window.location.pathname === "/feed" ? "active" : ""}>
+            <Link to="/feed">All</Link>
+          </li>
+          <li className={window.location.pathname === "/feed/delayfeed" ? "active" : ""}>
+            <Link to="/feed/delayfeed">Delay Posts</Link>
+          </li>
+          <li className={window.location.pathname === "/feed/station" ? "active" : ""}>
+            <Link to="/feed/station">Station Posts</Link>
+          </li>
+          <li className={window.location.pathname === "/feed/train" ? "active" : ""}>
+            <Link to="/feed/train">Train Posts</Link>
+          </li>
+          <li className={window.location.pathname === "/feed/myfeed" ? "active" : ""}>
+            <Link to="/feed/myfeed">My Posts</Link>
+          </li>
+        </ul>
+      </div>
+      <CreatePost />
     </div>
   </nav>;
 
